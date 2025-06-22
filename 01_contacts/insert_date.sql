28d425a931f2c2e1e8f6175ea0ae114c8bda1e77
alireza200411 <alirezamoradi292048@gmail.com>
2025-06-22 03:41:21 +0330

    update fitst practice

--- 01_contacts/insert_date.sql
+++ 01_contacts/insert_date.sql
@@ -2,4 +2,5 @@ INSERT INTO contacts
 VALUES
  	(1, 'Ali', '009891312345678', 'Ali@gmail.com'),
  	(2, 'Mahdi', '009892212345678', 'Mahdi@gmail.com'),
- 	(3, 'Liam', '0012025550181', 'Liam@gmail.com')
+ 	(3, 'Liam', '0012025550181', 'Liam@gmail.com'),
+	(4, 'Mohsen', '0012044450181', 'Mohsen@gmail.com')
--- 01_contacts/queries.sql
+++ 01_contacts/queries.sql
@@ -1,1 +1,14 @@
-SELECT * FROM contacts;
+SELECT name FROM contacts;
+
+
+SELECT * FROM contacts
+WHERE name = 'Ali';
+
+
+DELETE FROM contacts
+WHERE phone = '009892212345678';
+
+
+UPDATE contacts
+SET email = 'AliSoheili@gmail.com'
+WHERE name = 'Ali';
