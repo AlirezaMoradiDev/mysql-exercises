5a3b39b0f08e7059be0d05e1ca50836d750cf72c
alireza200411 <alirezamoradi292048@gmail.com>
2025-06-23 01:34:03 +0330

    add second practice

---
+++ 02_books/create.sql
@@ -0,0 +1,7 @@
+CREATE TABLE books(
+  id int PRIMARY KEY,
+  topic varchar(100),
+  author varchar(80),
+  publish_year year,
+  price decimal
+)
---
+++ 02_books/insert_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO books
+VALUES
+	(1, 'One Hundred Years of Solitude', 'Gabriel Garcia Marquez', '1967', 180000),
+    (2, 'Metamorphosis', 'Franz Kafka', '1915', 95000),
+    (3, 'The Little Prince', 'Antoine de Saint-Exupéry', '1943', 70000)
+  
+    
