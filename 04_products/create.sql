bf4bbe41f5c10279a0d89f5bb28f5facabc6b92b
alireza200411 <alirezamoradi292048@gmail.com>
2025-06-28 19:11:19 +0330

    add forth practic

---
+++ 04_products/create.sql
@@ -0,0 +1,6 @@
+CREATE TABLE products(
+    id INT PRIMARY KEY, 
+    name VARCHAR(60),
+    price DECIMAL,
+    stock INT
+);
---
+++ 04_products/insert_data.sql
@@ -0,0 +1,8 @@
+INSERT INTO products
+VALUES
+    (1, 'Asus Laptop', 45000000, 5),
+    (2, 'Sumsung A54', 18500000, 12),
+    (3, 'Bluetooth Headphones', 980000, 30),
+    (4, 'Wireless Mouse', 450000, 20),
+    (5, 'Mechanical Keyboard', 1300000, 15),
+    (6, '24-inch Monitor', 5200000, 0)
